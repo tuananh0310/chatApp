dc1eac26c46be1e44d87dfb75fda53228c5d798b
tuananh0310 <tuananh0310@gmail.com>
2022-06-19 23:57:39 +0700

    Updates

---
+++ precache-manifest.f761df42bf479385e48161a176d18a89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e993c95d04427adf4e1",
+    "url": "/chatApp/static/css/main.1b4c4675.chunk.css"
+  },
+  {
+    "revision": "9e993c95d04427adf4e1",
+    "url": "/chatApp/static/js/main.9e993c95.chunk.js"
+  },
+  {
+    "revision": "75aa2d861595ad62f4c5",
+    "url": "/chatApp/static/js/1.75aa2d86.chunk.js"
+  },
+  {
+    "revision": "08c1018c1aed23ddc716",
+    "url": "/chatApp/static/js/2.08c1018c.chunk.js"
+  },
+  {
+    "revision": "ec99173365a3c8018f07",
+    "url": "/chatApp/static/js/runtime~main.ec991733.js"
+  },
+  {
+    "revision": "6f81678461d80c30793dd3a3d1c82250",
+    "url": "/chatApp/index.html"
+  }
+];
